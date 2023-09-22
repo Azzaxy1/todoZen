d7acfb51c217e7031ebeda7138ae5567014c5bdd
Abdurrohman Azis <herdimamdra@gmail.com>
2023-09-23 02:28:12 +0700

    add all

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import TodoForm from "./components/TodoForm"
+import TodoWrapper from "./components/TodoWrapper"
+import TodoApp from "./data/TodoApp.json";
+
+
+function App() {
+
+  localStorage.setItem("todoAppData", JSON.stringify(TodoApp));
+
+  const getTodoAppData = () => {
+    let todoAppData = localStorage.getItem("todoAppData");
+    if (todoAppData) {
+      return (todoAppData = JSON.parse(localStorage.getItem("todoAppData")));
+    } else {
+      return [];
+    }
+  };
+  
+  const [data, setData] = useState(getTodoAppData());
+  useEffect(() => {
+    localStorage.setItem("todoAppData", JSON.stringify(data));
+  })
+  return (
+    <>
+      <div className="bg-cyan-400 flex justify-center items-center h-[100vh]">
+        <TodoWrapper data={data} setData={setData}/>
+      </div>
+    </>
+  )
+}
+
+export default App
